cc48c44b3765a68d90734cbb04262e47d72a1b60
rparham1997 <rparham1997@gmail.com>
2023-07-19 13:50:36 -0400

    Initial Commit

---
+++ Crypto-UI/Crypto_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_UIApp.swift
+//  Crypto-UI
+//
+//  Created by Ramar Parham on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
